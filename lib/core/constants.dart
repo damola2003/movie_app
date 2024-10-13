0c2704b6652817ddd675d38f91f47498f6bece1e
damola2003 <127348410+damola2003@users.noreply.github.com>
2024-10-13 08:07:29 +0100

    Update constants.dart

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class Constants {
-  static const apiKey = '98f86b0449db62b78f411cc68aae2b96';
+  static const apiKey = '';
   static const baseUrl = 'https://api.themoviedb.org/3/';
   static const imagePath = 'https://image.tmdb.org/t/p/w500/';
   static final headlineStyle = GoogleFonts.aBeeZee(
